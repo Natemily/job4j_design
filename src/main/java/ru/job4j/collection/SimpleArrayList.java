9122c62bcd42d6847239f465a8723fac05804b8a
Natemily <nerezko.n@gmail.com>
2022-07-22 14:52:37 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,80 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            container = Arrays.copyOf(container, size * 2);
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size);
+        T temp = container[index];
+        container[index] = newValue;
+        modCount++;
+        return temp;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T temp = container[index];
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        container[container.length - 1] = null;
+        size--;
+        modCount++;
+        return temp;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int i = 0;
+            private int j = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (j != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return i < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[i++];
+            }
+        };
+    }
+}
