81ad3ac1a968f05e84c4eee91642b72fc1a0cf3e
Natemily <nerezko.n@gmail.com>
2023-03-30 15:42:51 +0300

    7. Анализ доступности сервера [#859 #405547]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,29 +6,19 @@ import java.util.List;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        List<String> temp = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+        String previous = null;
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (String line = in.readLine(); line != null; line = in.readLine()) {
-                temp.add(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
-            for (int i = 0; i < temp.size(); i++) {
-                String value = temp.get(i);
-                String valueNext = ((i + 1) < temp.size()) ? temp.get(i + 1) : (null);
-                if (value.startsWith("400") || value.startsWith("500")) {
-                    String[] array = value.split(" ");
+                if (line.startsWith("400") || line.startsWith("500")) {
+                    String[] array = line.split(" ");
                     out.print(array[1] + "; ");
-                    if ((valueNext != null && !valueNext.startsWith("400"))
-                        || (valueNext != null && !valueNext.startsWith("500"))) {
-                    String[] arrayNext = valueNext.split(" ");
-                    out.println(arrayNext[1] + ";");
-                    }
+                    previous = line;
+                } else if ((!line.startsWith("400") && previous != null)
+                        || (!line.startsWith("500") && previous != null)) {
+                    String[] array = line.split(" ");
+                    out.println(array[1] + ";");
+                    previous = null;
                 }
             }
         } catch (IOException e) {
