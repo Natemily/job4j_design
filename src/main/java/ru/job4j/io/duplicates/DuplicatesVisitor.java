e060cd52018c36fc07391f94644bfb0f42512754
Natemily <nerezko.n@gmail.com>
2023-06-07 17:33:31 +0300

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,36 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+
+    Map<FileProperty, Set<String>> filesList = new HashMap<>();
+
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (attrs.isRegularFile()) {
+            FileProperty fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
+            filesList.computeIfAbsent(fileProperty, k -> new HashSet<>()).add(file.toAbsolutePath().toString());
+        }
+        return super.visitFile(file, attrs);
+    }
+
+    public Map<FileProperty, Set<String>> helper() {
+        Map<FileProperty, Set<String>> filesListTotal = new HashMap<>();
+        for (FileProperty key : filesList.keySet()) {
+            if (filesList.get(key).size() > 1) {
+                filesListTotal.put(key, filesList.get(key));
+            }
+        }
+        return filesListTotal;
+    }
+}
