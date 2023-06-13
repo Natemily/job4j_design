329367ffbaaf93668030b1f762ac74978f609425
Natemily <nerezko.n@gmail.com>
2023-06-13 14:50:31 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,8 +9,21 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("h:/Нерезько Н/!Гиприк/Java/projects");
-        search(start, p -> p.toFile().getName().endsWith(".iml")).forEach(System.out::println);
+        valid(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    public static void valid(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Должно быть два аргумента.");
+        }
+        if (!Files.exists(Paths.get(args[0]))) {
+            throw new IllegalArgumentException("Папка должна существовать. Проверьте ее наличие.");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Раcширение файлов должно начинаться с точки");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
