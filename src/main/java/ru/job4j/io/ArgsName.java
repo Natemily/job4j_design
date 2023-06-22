ed5476d5db0402c7d31969a483e3532041fc4b26
Natemily <nerezko.n@gmail.com>
2023-06-22 11:12:24 +0300

    13. Именованные аргументы [#295518 #424557]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -10,31 +10,31 @@ public class ArgsName {
 
     public String get(String key) {
         if (!values.containsKey(key)) {
-            throw new IllegalArgumentException("This key: '" + key + "' is missing");
+            throw new IllegalArgumentException(String.format("This key: '%s' is missing", key));
         }
         return values.get(key);
     }
 
     private void parse(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            String[] temp = args[i].split("=", 2);
-            valid(args[i], temp);
+        for (String arg : args) {
+            String[] temp = arg.split("=", 2);
+            valid(arg, temp);
             values.put(temp[0].substring(1), temp[1]);
         }
     }
 
     private void valid(String arg, String[] temp) {
         if (!arg.contains("=")) {
-            throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain an equal sign");
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain an equal sign", arg));
         }
         if (temp[0].equals("-")) {
-            throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain a key");
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain a key", arg));
         }
         if (temp[1].equals("")) {
-            throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain a value");
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain a value", arg));
         }
         if (!temp[0].startsWith("-")) {
-            throw new IllegalArgumentException("Error: This argument '" + arg + "' does not start with a '-' character");
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not start with a '-' character", arg));
         }
     }
 
