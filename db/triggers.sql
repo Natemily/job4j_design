1729a2970205a56a2973582ea4063f7eb20aff6a
Natemily <nerezko.n@gmail.com>
2023-09-07 14:58:16 +0300

    2. Триггеры [#504803]

---
+++ db/triggers.sql
@@ -0,0 +1,82 @@
+create table products (
+    id serial primary key,
+    name varchar(50),
+    producer varchar(50),
+    count integer default 0,
+    price integer
+);
+
+	create or replace function tax()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price + price * 0.2
+        where id = (select id from inserted);
+        return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_trigger
+    after insert on products
+    referencing new table as inserted
+    for each statement
+    execute procedure tax();
+
+insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 5, 100);
+insert into products (name, producer, count, price) VALUES ('product_2', 'producer_2', 8, 120);
+
+select * from products;
+
+drop trigger tax_trigger on products;
+
+create or replace function tax_before()
+    returns trigger as
+$$
+    BEGIN
+        new.price = new.price + new.price * 0.2;
+        return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_before_trigger
+    before insert
+    on products
+    for each row
+    execute procedure tax_before();
+
+insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 3, 150);
+insert into products (name, producer, count, price) VALUES ('product_2', 'producer_2', 1, 200);
+
+select * from products;
+
+create table history_of_price (
+    id serial primary key,
+    name varchar(50),
+    price integer,
+    date timestamp
+);
+
+create or replace function add_history()
+    returns trigger as
+$$
+    BEGIN
+        insert into "history_of_price" (name, price, date)
+		values(new.name, new.price, current_date);
+        return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger add_history_a_table
+    after insert
+    on products
+    for each row
+    execute procedure add_history();
+
+insert into products (name, producer, count, price) VALUES ('product_3', 'producer_3', 10, 350);
+
+select * from products;
+select * from history_of_price;
