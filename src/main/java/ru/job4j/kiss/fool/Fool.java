671e6ef81eed05085c7faa66451589c464a8a71e
Natemily <nerezko.n@gmail.com>
2024-02-07 14:31:22 +0300

    2. Принципы Kiss, Dry и Yagni [#238813]

--- src/main/java/ru/job4j/kiss/fool/Fool.java
+++ src/main/java/ru/job4j/kiss/fool/Fool.java
@@ -11,34 +11,18 @@ public class Fool {
         return value % 5 == 0;
     }
 
-    public static void computerTurn(int startAt) {
-        if (check3(startAt) && check5(startAt)) {
-            System.out.println("FizzBuzz");
-        } else if (check3(startAt)) {
-            System.out.println("Fizz");
-        } else if (check5(startAt)) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(startAt);
+    public static String answer(int startAt) {
+        String answer = "";
+        if (!check3(startAt) && !check5(startAt)) {
+            answer += startAt;
         }
-    }
-
-    public static boolean userAnswer(String answer, int startAt) {
-        String expected;
-        boolean res = true;
-        if (check3(startAt) && check5(startAt)) {
-            expected = "FizzBuzz";
-        } else if (check3(startAt)) {
-            expected = "Fizz";
-        } else if (check5(startAt)) {
-            expected = "Buzz";
-        } else {
-            expected = String.valueOf(startAt);
+        if (check3(startAt)) {
+            answer += "Fizz";
         }
-        if (!expected.equals(answer)) {
-            res = false;
+        if (check5(startAt)) {
+            answer += "Buzz";
         }
-        return res;
+        return answer;
     }
 
     public static void main(String[] args) {
@@ -46,10 +30,10 @@ public class Fool {
         var startAt = 1;
         var input = new Scanner(System.in);
         while (startAt < 100) {
-            computerTurn(startAt);
+            System.out.println(answer(startAt));
             startAt++;
-            var answer = input.nextLine();
-            if (!userAnswer(answer, startAt)) {
+            var userAnswer = input.nextLine();
+            if (!answer(startAt).equals(userAnswer)) {
                 System.out.println("Ошибка. Начинай снова.");
                 startAt = 0;
             }
--- src/test/java/ru/job4j/kiss/fool/FoolTest.java
+++ src/test/java/ru/job4j/kiss/fool/FoolTest.java
@@ -8,39 +8,25 @@ class FoolTest {
 
     @Test
     void test3() {
-        assertThat(Fool.userAnswer("Fizz", 3)).isTrue();
-        assertThat(Fool.userAnswer("3", 3)).isFalse();
-        assertThat(Fool.userAnswer("Buzz", 3)).isFalse();
-        assertThat(Fool.userAnswer("Fizz", 9)).isTrue();
-        assertThat(Fool.userAnswer("FizzBuzz", 6)).isFalse();
-        assertThat(Fool.userAnswer("+", 6)).isFalse();
+        assertThat(Fool.answer(3)).isEqualTo("Fizz");
+        assertThat(Fool.answer(6)).isEqualTo("Fizz");
     }
 
     @Test
     void test5() {
-        assertThat(Fool.userAnswer("Buzz", 5)).isTrue();
-        assertThat(Fool.userAnswer("5", 5)).isFalse();
-        assertThat(Fool.userAnswer("Fizz", 5)).isFalse();
-        assertThat(Fool.userAnswer("Buzz", 20)).isTrue();
-        assertThat(Fool.userAnswer("FizzBuzz", 25)).isFalse();
-        assertThat(Fool.userAnswer("+", 25)).isFalse();
+        assertThat(Fool.answer(5)).isEqualTo("Buzz");
+        assertThat(Fool.answer(10)).isEqualTo("Buzz");
     }
 
     @Test
     void test35() {
-        assertThat(Fool.userAnswer("FizzBuzz", 15)).isTrue();
-        assertThat(Fool.userAnswer("15", 15)).isFalse();
-        assertThat(Fool.userAnswer("Buzz", 15)).isFalse();
-        assertThat(Fool.userAnswer("Fizz", 45)).isFalse();
-        assertThat(Fool.userAnswer("FizzBuzz", 15)).isTrue();
-        assertThat(Fool.userAnswer("+", 30)).isFalse();
+        assertThat(Fool.answer(15)).isEqualTo("FizzBuzz");
+        assertThat(Fool.answer(45)).isEqualTo("FizzBuzz");
     }
 
     @Test
     void testGeneral() {
-        assertThat(Fool.userAnswer("4", 4)).isTrue();
-        assertThat(Fool.userAnswer("15", 15)).isFalse();
-        assertThat(Fool.userAnswer("3", 7)).isFalse();
-        assertThat(Fool.userAnswer("+", 17)).isFalse();
+        assertThat(Fool.answer(7)).isEqualTo("7");
+        assertThat(Fool.answer(8)).isEqualTo("8");
     }
 }
